c44a6507bad6bb819dbe37e1913291d2bbc1c846
LitoMore <litomore@gmail.com>
2021-01-14 21:31:41 +0800

    Add Box component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Slide from "./components/slide";
 import Product from "./components/product";
 import Open from "./components/open";
+import Box from "./components/box";
 import Join from "./components/join";
 import Footer from "./components/footer";
 
@@ -12,6 +13,7 @@ const App = () => {
 			<Slide />
 			<Product />
 			<Open />
+			<Box />
 			<Join />
 			<Footer />
 		</>
---
+++ src/components/box.js
@@ -0,0 +1,15 @@
+import "./box.css";
+
+const Box = () => {
+	return (
+		<div className="box">
+			<div className="iphone">
+				<div className="chat left">您好，欢迎使用医疗魔方</div>
+				<div className="chat right">分析我的数据</div>
+				<div className="chat left">正在分析</div>
+			</div>
+		</div>
+	);
+};
+
+export default Box;
