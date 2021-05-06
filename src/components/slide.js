72922ba60ff1517fc80c817515062d0afaecaaff
LitoMore <litomore@gmail.com>
2021-05-06 19:50:57 +0800

    Update copywriting

--- src/components/slide.js
+++ src/components/slide.js
@@ -1,7 +1,6 @@
 import Slider from "react-slick";
 import s1 from "../assets/slide-1.png";
 import s2 from "../assets/slide-2.png";
-import HalfCircle from "../assets/half-circle";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./slide.css";
@@ -46,16 +45,16 @@ const Slide = () => (
 				image={s2}
 				l1={
 					<>
-						{"打造\n"}有益於<span className="highlight">人类健康</span>的
+						{"引领\n"}
+						<span className="highlight">智慧医疗</span>
 					</>
 				}
-				l2={"AI"}
+				l2={"革新"}
 				l3={
-					"未然科技专注于人工智能在疾病预测领域的技术创新，通过运用机器学习，深度学习等人工智能技术对健康，医疗大数据进行数据挖掘，拥有智能辅助诊疗等服务"
+					"未然科技旨在全面打造智慧医疗生态环境，为用户提供全新的智能自诊，健康管理，远程医疗体验"
 				}
 			/>
 		</Slider>
-		<HalfCircle className="half-circle" stroke="#2f80ed" />
 	</div>
 );
 
